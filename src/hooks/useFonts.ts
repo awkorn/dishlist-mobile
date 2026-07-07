944e2ab6f7eafb946db949aaa2abf9ca80df2ee5
Alex Korn <alexk9255@gmail.com>
2026-07-06 21:38:03 -0500

    remove unused font

--- src/hooks/useFonts.ts
+++ src/hooks/useFonts.ts
@@ -2,11 +2,6 @@ import { useFonts } from "expo-font";
 
 export const useCustomFonts = () => {
     const [fontsLoaded] = useFonts({
-        'Bricolage-Regular': require('../../assets/fonts/BricolageGrotesque-Regular.ttf'),
-        'Bricolage-Bold': require('../../assets/fonts/BricolageGrotesque-Bold.ttf'),
-        'Bricolage-Light': require('../../assets/fonts/BricolageGrotesque-Light.ttf'),
-        'Bricolage-SemiBold': require('../../assets/fonts/BricolageGrotesque-SemiBold.ttf'),
-        
         'GeneralSans-SemiBold': require('../../assets/fonts/GeneralSans-Semibold.ttf'),
 
         'PlayfairDisplay-Regular': require('../../assets/fonts/PlayfairDisplay-Regular.ttf'),
